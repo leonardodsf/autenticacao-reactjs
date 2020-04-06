17c376b6ca636c8f773ff9418cacebb27049a499
Leonardo Flores <contatoleonardoflores@gmail.com>
2020-04-06 01:04:31 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
-    {...rest}
-    render={(props) =>
+    { ...rest }
+    render={( props ) =>
       isAuthenticated() ? (
-        <Component {...props} />
+        <Component { ...props } />
       ) : (
         <Redirect to={{ pathname: "/", state: { from: props.location } }} />
       )
